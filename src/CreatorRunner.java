5c8db4662e44e17d12d371d4feaa63162e7dc6f6
Kirill <seeraydary@gmail.com>
2024-02-06 15:00:42 +0300

    Release 1.0

--- src/CreatorRunner.java
+++ src/CreatorRunner.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class CreatorRunner {
     public static void main(String[] args) throws IOException {
         //Добавить в 'challenge' ID или название задачи
-        String challenge = "541c8630095125aba6000c00";
+        String challenge = "59325dc15dbb44b2440000af";
         String url = "https://www.codewars.com/api/v1/code-challenges/" + challenge;
         Creator creator = new Creator();
         creator.start(url);
---
+++ src/util/DevClass.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Класс для разработки и тестирования будущего функционала
+ */
+
+public class DevClass {
+    public static void main(String[] args)  {
+
+    }
+}
