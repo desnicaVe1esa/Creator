4787912ccae96bf0e872b9eee052dcc1c5df0358
Kirill <seeraydary@gmail.com>
2024-07-18 09:25:41 +0300

    add data at work

---
+++ back/src/main/resources/db/migration/V18__add_data_at_work.sql
@@ -0,0 +1,5 @@
+insert into work (publication_date, info)
+values ('2024-07-18', 'JavaScript. Запись +<something> - преобразование в число\nПример:\nreturn +result;');
+
+insert into work (publication_date, info)
+values ('2024-07-18', 'JavaScript. Запись <expression> | 0 - приведение к целому типу\nПример:\nnum1 = num1 / 10 | 0; // 123.4 | 0 === 123\nЗапись <expression> | 1 - приведение к целому типу + 1\nПример:\nnum1 = num1 / 10 | 1; // 123.4 | 1 === 124');
