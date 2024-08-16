47560592b62a54f2ff3d658c32c001d2a7b68b09
Kirill <seeraydary@gmail.com>
2024-08-16 15:17:23 +0300

    added patterns theory

---
+++ front/src/page/theory/component/patternsComponent/index.js
@@ -0,0 +1,173 @@
+import {Col, Row} from "antd";
+
+const PatternsComponent = () => {
+    return (
+        <div>
+            {/** Фабричный метод */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/factory_method/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={600} height={350} src={'patterns/factory_method/factory_method.jpg'}
+                         alt={'factory_method'}></img>
+                    <img width={600} height={350} src={'patterns/factory_method/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Абстрактная фабрика */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/abstract_factory/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={600} height={350} src={'patterns/abstract_factory/abstract_factory.jpg'}
+                         alt={'abstract_factory'}></img>
+                    <img width={600} height={350} src={'patterns/abstract_factory/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Строитель */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/builder/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={600} height={500} src={'patterns/builder/builder.jpg'} alt={'builder'}></img>
+                    <img width={600} height={500} src={'patterns/builder/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Прототип */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/prototype/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={400} height={300} src={'patterns/prototype/prototype.jpg'} alt={'prototype'}></img>
+                    <img width={400} height={300} src={'patterns/prototype/prototype_storage.jpg'}
+                         alt={'prototype_storage'}></img>
+                    <img width={400} height={300} src={'patterns/prototype/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Одиночка */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <img src={'patterns/singleton/name.jpg'} alt={'name'}></img>
+            </Row>
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <img width={600} height={350} src={'patterns/singleton/singleton.jpg'} alt={'singleton'}></img>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Адаптер */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/adapter/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={350} height={300} src={'patterns/adapter/adapter_classes.jpg'} alt={'adapter_classes'}></img>
+                    <img width={350} height={300} src={'patterns/adapter/adapter_objects.jpg'}
+                         alt={'adapter_objects'}></img>
+                    <img width={500} height={300} src={'patterns/adapter/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Мост */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/bridge/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={600} height={400} src={'patterns/bridge/bridge.jpg'} alt={'bridge'}></img>
+                    <img width={600} height={400} src={'patterns/bridge/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Компоновщик */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/composite/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={500} height={400} src={'patterns/composite/composite.jpg'} alt={'composite'}></img>
+                    <img width={500} height={400} src={'patterns/composite/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Декоратор */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/decorator/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={500} height={400} src={'patterns/decorator/decorator.jpg'} alt={'decorator'}></img>
+                    <img width={500} height={400} src={'patterns/decorator/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            {/** Фасад */}
+            <Row style={{display: 'flex', justifyContent: 'center'}}>
+                <Col>
+                    <img src={'patterns/facade/name.jpg'} alt={'name'}></img>
+                </Col>
+                <Col>
+                    <img width={600} height={350} src={'patterns/facade/facade.jpg'} alt={'facade'}></img>
+                    <img width={600} height={350} src={'patterns/facade/example.jpg'} alt={'example'}></img>
+                </Col>
+            </Row>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+        </div>
+    )
+}
+
+export default PatternsComponent;
--- front/src/page/theory/index.js
+++ front/src/page/theory/index.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {HomeOutlined, LogoutOutlined, LaptopOutlined} from '@ant-design/icons';
+import {HomeOutlined, LogoutOutlined, LaptopOutlined, BuildOutlined} from '@ant-design/icons';
 import {Menu} from "antd";
 import {Content} from "antd/es/layout/layout";
 import ContentComponent from "./component/contentComponent";
+import PatternsComponent from "./component/patternsComponent";
 
 function getItem(label, key, icon, children) {
     return {
@@ -16,6 +17,7 @@ function getItem(label, key, icon, children) {
 const items = [
     getItem('Домашняя практика', 'homePractice', <HomeOutlined/>),
     getItem('Работа', 'work', <LaptopOutlined/>),
+    getItem('Паттерны проектирования', 'patterns', <BuildOutlined/>),
     getItem('На главную', 'main', <LogoutOutlined/>)
 ];
 
@@ -24,15 +26,23 @@ const Theory = () => {
     const [key, setKey] = useState('');
     const [homePractice, setHomePractice] = useState([]);
     const [work, setWork] = useState([]);
+    const [backGroundImage, setBackGroundImage] = useState('');
 
     const onMenuClick = (e) => {
         if (e.key === 'main') {
             return window.location.replace('http://localhost:3000/main');
         }
+
         if (e.key === key) {
             setKey('');
         } else {
             setKey(e.key);
+        }
+
+        if (e.key !== 'patterns') {
+            setBackGroundImage(`url(${process.env.PUBLIC_URL + 'img/background_theory.jpg'})`)
+        } else {
+            setBackGroundImage('')
         }
     }
 
@@ -62,7 +72,7 @@ const Theory = () => {
         <div style={{
             display: 'flex',
             flexDirection: 'row',
-            backgroundImage: `url(${process.env.PUBLIC_URL + 'img/background_theory.jpg'})`,
+            backgroundImage: backGroundImage,
             backgroundPosition: 'center',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
@@ -76,12 +86,15 @@ const Theory = () => {
                   onClick={(key) => onMenuClick(key)}
                   theme="dark"
                   items={items}/>
-            <Content style={{marginLeft: '13%'}}>
+            <Content style={{marginLeft: '15.5%'}}>
                 {key === 'homePractice' &&
                     <ContentComponent params={homePractice}/>
                 }
                 {key === 'work' &&
                     <ContentComponent params={work}/>
+                }
+                {key === 'patterns' &&
+                    <PatternsComponent />
                 }
             </Content>
         </div>
